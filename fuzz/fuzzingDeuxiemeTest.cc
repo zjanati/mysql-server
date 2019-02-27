22b186ebf21973b65d866293972a01057c4ec2c3
Christopher Alves <christopher.alves@telecomnancy.eu>
2019-02-27 13:53:32 +0100

    Basculement sur branche fuzzing

---
+++ fuzz/fuzzingDeuxiemeTest.cc
@@ -0,0 +1,29 @@
+#include <exception>
+
+#include <mysql.h>
+#include <mysql/client_plugin.h>
+#include <mysqld_error.h>
+
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    //Let's init...
+    MYSQL *mysql;
+    MY_INIT();
+    if (mysql_server_init(0, NULL, NULL)) return 1;
+    mysql = client_connect(0, MYSQL_PROTOCOL_DEFAULT, 1);
+    
+    //Null character in the middle of data ; splitting them to make several queries
+    //Here is the test
+std:string dataAsString(Data, Data + Size);
+    MYSQL_STMT *stmt;
+    stmt = mysql_simple_prepare(mysql, dataAsString);
+    check_stmt(stmt);
+    
+    
+    //Let's stop...
+    client_disconnect(mysql);
+    mysql_server_end();
+    my_end(0);
+    
+    return 0;
+}
